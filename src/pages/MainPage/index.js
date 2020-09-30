49ac0ca895ee5088bcb58574177284a683ee350e
Joel Sebastião Mbengui <50587674+JSMbengui@users.noreply.github.com>
2020-09-30 14:42:02 +0100

    Terminada o design da pagina principal. vV0.0.1

--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import './index.css';
+import { GrCode, GrGithub, GrLinkedin } from 'react-icons/gr';
 
 const MainPage = () => {
-    
+
     return (
-        null
+        <>
+            <main className="main">
+                <section className="main-about">
+                    <div>
+                        <h1>JOEL S. MBENGUI</h1>
+                        <p>Software Developer</p>
+                    </div>
+                    <div className="service">
+                        <p style={{ color: "#530082" }}><GrCode size={'12rem'} /></p>
+                    </div>
+                </section>
+            </main>
+            <footer>
+                <ul className="d-flex-center">
+                    <li><a href="https://github.com/JSMbengui" target="_blank"  rel="noopener noreferrer"><GrGithub /></a></li>
+                    <li><a href="https://www.linkedin.com/in/joel-sebasti%C3%A3o-mbengui/" target="_blank"  rel="noopener noreferrer"><GrLinkedin /></a></li>
+                </ul>
+            </footer>
+        </>
     )
 }
 
